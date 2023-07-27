80ef97d6150f85cfc82ffcf33886681bb591faee
Бархүү Хөлөгжин <HP233037@M301-CC0124.local>
2023-07-27 13:12:37 +0800

    done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,118 +1,141 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import { useState, useEffect } from "react";
 
-const inter = Inter({ subsets: ['latin'] })
+const SnakeGame = () => {
+  const [snakeDirection, setSnakeDirection] = useState("right");
+  const [headLeft, setHeadLeft] = useState(0);
+  const [headDown, setHeadDown] = useState(0);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      switch (snakeDirection) {
+        case "up":
+          setHeadDown((prevHeadDown) => prevHeadDown - 15);
+          break;
+        case "left":
+          setHeadLeft((prevHeadLeft) => prevHeadLeft - 15);
+          break;
+        case "down":
+          setHeadDown((prevHeadDown) => prevHeadDown + 15);
+          break;
+        case "right":
+          setHeadLeft((prevHeadLeft) => prevHeadLeft + 15);
+          break;
+        default:
+          break;
+      }
+    }, 100);
 
-export default function Home() {
+    return () => clearInterval(interval);
+  }, [snakeDirection]);
+
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      switch (event.key) {
+        case "w":
+          setSnakeDirection("up");
+          break;
+        case "a":
+          setSnakeDirection("left");
+          break;
+        case "s":
+          setSnakeDirection("down");
+          break;
+        case "d":
+          setSnakeDirection("right");
+          break;
+        default:
+          break;
+      }
+    };
+
+    document.addEventListener("keydown", handleKeyDown);
+
+    return () => {
+      document.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
+  const maxX = 380;
+  const maxY = 380;
+  const [genX, setGenX] = useState(0);
+  const [genY, setGenY] = useState(0);
+  const [score, setScore] = useState(-2);
+
+  useEffect(() => {
+    setGenX(Math.floor(Math.random() * 380));
+    setGenY(Math.floor(Math.random() * 380));
+    console.log(genX, genY);
+  }, []);
+
+  const boundedHeadLeft = Math.min(Math.max(0, headLeft), maxX);
+  const boundedHeadDown = Math.min(Math.max(0, headDown), maxY);
+
+  console.log(boundedHeadLeft, boundedHeadDown);
+
+  useEffect(() => {
+    if (boundedHeadDown === genX || boundedHeadLeft === genY) {
+      setScore((score) => score + 1);
+    }
+  }, [boundedHeadLeft, boundedHeadDown]);
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <div>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          width: "100%",
+          height: "100vh",
+          alignItems: "center",
+          flexDirection: "column",
+        }}
+      >
+        <div>{score}</div>
+        <button
+          style={{
+            marginBottom: 30,
+            border: "2px solid black",
+            borderRadius: "10px",
+          }}
+          // onClick={}
+        >
+          <div style={{ padding: 5 }}> Start</div>
+        </button>
+        <div
+          style={{
+            width: "400px",
+            height: "400px",
+            backgroundColor: "aqua",
+            display: "flex",
+            position: "relative",
+          }}
+        >
+          <div
+            style={{
+              width: "20px",
+              height: "20px",
+              borderRadius: "5px",
+              backgroundColor: "transparent",
+              position: "absolute",
+              top: `${genY}px`,
+              left: `${genX}px`,
+            }}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            🍎
+          </div>
+          <div
+            style={{
+              width: "20px",
+              height: "20px",
+              borderRadius: "5px",
+              backgroundColor: "black",
+              position: "absolute",
+              top: `${boundedHeadDown}px`,
+              left: `${boundedHeadLeft}px`,
+            }}
+          />
         </div>
       </div>
+    </div>
+  );
+};
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
-}
+export default SnakeGame;
